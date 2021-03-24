67e858acd14cc61949383dcddbccc04919983ce2
krynju <gulinski.krystian@gmail.com>
2021-03-25 00:52:28 +0100

    a

--- src/MH.jl
+++ src/MH.jl
@@ -225,6 +225,7 @@ using CUDA, Random123, StructArrays
 
 
 
+
 function mh_gpu(
     x₀::T,
     N::Integer,
@@ -240,19 +241,6 @@ function mh_gpu(
         f_xc = f(xc)
         α = f_xc / f_xₜ
         xc, α, f_xc
-    end
-
-    function __generate_loop(x::Vector{T}, xₜ::T, σ::T, range::R, f, rng::RNG) where {T <: AbstractFloat,R <: AbstractRange,RNG <: AbstractRNG} 
-        f_xₜ = f(xₜ)
-        for t in range
-            xc, α, f_xc = _gen_candidate(xₜ, σ, f_xₜ, f, rng)
-            u = rand(rng, Uniform(0, 1))
-            if (u <= α)
-                xₜ = xc
-                f_xₜ = f_xc
-            end
-            @inbounds x[t] = xₜ
-        end
     end
 
     function __burn_loop(xₜ::T, σ::T, burn_N::Integer, target, f, rng::RNG) where  {T <: AbstractFloat,RNG <: AbstractRNG} 
@@ -271,7 +259,6 @@ function mh_gpu(
         σ, xₜ
     end
 
-    x = CuArray{T}(undef, N)
     xₜ = x₀::T
     σ = one(T)
 
@@ -280,35 +267,26 @@ function mh_gpu(
     TARGET = 0.3
     σ, xₜ = __burn_loop(xₜ, σ, burn_N, TARGET, f, rng)
 
-    _n = (_a, _b) -> (_a + _b - 1) ÷ _b
 
-    SAMPLES_PER_THREAD = 10000
-    N_THR_PER_BLOCK = 1024
-    N_BLOCKS = _n(N, SAMPLES_PER_THREAD * N_THR_PER_BLOCK)
-    N_THR = _n(N, SAMPLES_PER_THREAD)
-
-    function my_pdf(mean, std, x)
-        return convert(Float32, 0.3989481634448608f0 / std * exp(-0.5 * (((x - mean) / std)^2)));
+    function d_pdf(mean, std, x)
+        0.3989481634448608f0 / std * exp(-0.5 * (((x - mean) / std)^2))
     end
     
     function d_ff(x)
         x /= 100.0
-        my_pdf(0.0, 1.0, x) + my_pdf(3.0, 1.0, x) + my_pdf(6.0, 1.0, x)
+        d_pdf(0.0, 1.0, x) + d_pdf(3.0, 1.0, x) + d_pdf(6.0, 1.0, x)
     end
 
-    function d__generate_loop2(x, xₜ, σ, N, rng)
+    function d_generate_loop(x, xₜ, σ, N, rng)
         id = (blockIdx().x - 1) * blockDim().x + threadIdx().x
         range = (1 + (id - 1) * SAMPLES_PER_THREAD):(id * SAMPLES_PER_THREAD) % (N + 1)
-      
         if range.start <= range.stop
             r =  rng[id]
             set_counter!(r, (id - 1) * 4 * SAMPLES_PER_THREAD)
             f_xₜ = d_ff(xₜ)
             for t in range
-                ll = -2.0f0 * CUDA.log(rand(r, Float32))
-                cc = CUDA.cos(2.0f0 * π * rand(r, Float32))
-                n = CUDA.sqrt(ll) * cc
-            
+                n = CUDA.sqrt(-2f0 * CUDA.log(rand(r, Float32))) *  CUDA.cos(2f0 * π * rand(r, Float32))
+              
                 xc = xₜ  +  σ  * n
                 f_xc = d_ff(xc)
                 α = f_xc / f_xₜ
@@ -317,21 +295,28 @@ function mh_gpu(
                     xₜ = xc
                     f_xₜ = f_xc
                 end
-                x[t] = xₜ
+                @inbounds x[t] = xₜ
             end
         end
         return nothing
     end
 
-    a = StructArray([Philox2x(UInt32, 1337) for _ in 1:N_THR])
-    b = replace_storage(CuArray, a)
+    _n = (_a, _b) -> (_a + _b - 1) ÷ _b
 
+    SAMPLES_PER_THREAD = 100000
+    N_THR_PER_BLOCK = 1024
+    N_BLOCKS = _n(N, SAMPLES_PER_THREAD * N_THR_PER_BLOCK)
+    N_THR = _n(N, SAMPLES_PER_THREAD)
+
+    d_x = CuArray{T}(undef, N)
+    generators = StructArray([Philox2x(UInt32, seed + i) for i in 1:N_THR])
+    d_generators = replace_storage(CuArray, generators)
 
     CUDA.@sync begin
-        @cuda threads = N_THR_PER_BLOCK blocks = N_BLOCKS d__generate_loop2(x, xₜ, σ, N, b)
+        @cuda threads = N_THR_PER_BLOCK blocks = N_BLOCKS d_generate_loop(d_x, xₜ, σ, N, d_generators)
     end
     
-    return x
+    return Vector{T}(d_x)
 end
 
 export ff, mh_serial_naive, mh_serial_optimized, mh_threaded_naive, mh_threaded_optimized , mh_gpu
